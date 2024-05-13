d5a6cdaa93ff62382834702b6e975b81c9763d15
Gabriel MARIE <gaby01@youmail.fr>
2024-05-13 17:04:13 +0200

    adding character movement

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    public float speed = 5f;
+    public Animator animator;
+
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        animator.SetFloat("Horizontal", Input.GetAxis("Horizontal"));
+        animator.SetFloat("Vertical", Input.GetAxis("Vertical"));
+
+        Vector3 movement = new Vector3(horizontalInput, 0f, verticalInput) * speed * Time.deltaTime;
+
+        transform.Translate(movement);
+    }
+}
