b4e782255e0f634f091e6be13beaef93bca8f9bc
Gabriel MARIE <gaby01@youmail.fr>
2024-05-16 17:41:20 +0200

    adding

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -4,6 +4,18 @@ public class CharacterController : MonoBehaviour
 {
     public float speed = 5f;
     public Animator animator;
+    public CharacterController characterController;
+    public Vector3 playerVelocity;
+    public bool groundedPlayer;
+    [SerializeField] public float jumpHeight = 5.0f;
+    public bool jumpPressed = false;
+    public float gravity = -9.81f;
+
+
+    private void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
 
     void Update()
     {
@@ -17,4 +29,13 @@ public class CharacterController : MonoBehaviour
 
         transform.Translate(movement);
     }
+
+    void Jump()
+    {
+        if (!groundedPlayer)
+        {
+
+        }
+    }
+
 }
